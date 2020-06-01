3718a24385ce9a0e13f4a7bd3526ad0cdb4cab11
Elisha49 <elishae207@gmail.com>
2020-06-01 17:58:16 +1000

    part2   1,2,3& 4

--- library.R
+++ library.R
@@ -7,14 +7,11 @@ library("Biostrings")
 #To download the e.coli file
 download.file("ftp://ftp.ensemblgenomes.org/pub/bacteria/release-42/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/cds/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz",
               destfile = "e.coli.fa.gz")
-R.utils::gunzip("Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa.gz", overwrite= TRUE)
-unlink("e.coli.fa")
-gunzip("e.coli.fa.gz")
-
+R.utils::gunzip("e.coli.fa.gz", overwrite= TRUE)
 makeblastdb("e.coli.fa",dbtype="nucl","-parse_seqids")
 #To download sample file
 download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/sample.fa",
-              destfile = ("sample.fa"))
+              destfile = "sample.fa")
 x <- read.fasta("sample.fa")
 myseq <- x[[2]]
 myseq
@@ -22,11 +19,10 @@ str(myseq)
 seqinr::getLength(myseq)
 length(myseq)
 seqinr::GC(myseq)
+download.file("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/mutblast_functions.R",
+                  destfile = "mutblast.R")
 
-download.file("https://github.com/markziemann/SLE712_files/blob/master/bioinfo_asst3_part2_files/mutblast_functions.R",
-              destfile = "mutblast.R")
-
-
+source("mutblast.R")
 
 
 
@@ -62,8 +58,19 @@ hits
 db <- read.fasta("e.coli.fa")
 str(db[1:6])
 head(names(db))
+
+myseqs <- db[which(names(db)%in%hits)]
+myseqs <- c(myseqs,x)
+seqinr::write.fasta(tophit,names = names(myseq),file.out = "myseqs.fa")
+str(myseqs)
+
 tophit <- db[which(names(db) %in% hits[1])]
 tophit[1:3]
+seqinr::write.fasta(tophit,names=names(tophit),file.out = "tophit.fa")
+str(tophit)
+
+makeblastdb("tophit.fa",dbtype="nucl", "-parse_seqids" )
+res <- myblastn(myseq = x, db = "tophit.fa" )
 res
 
 
@@ -74,8 +81,42 @@ mutator <- function(myseq,nmut) {
   return(myseq_mod)
 }
 
+#qsn 4
+source("https://raw.githubusercontent.com/markziemann/SLE712_files/master/bioinfo_asst3_part2_files/mutblast_functions.R")
+xmutator <- mutator(x,2)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,3)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,5)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,10)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,20)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,25)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,30)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,50)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,60)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
+xmutator <- mutator(x,70)
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
 
+xmutator <- mutator(x,100)
 
-
+res <- myblastn_tab(myseq=xmutator, db ="tophit.fa")
+res
 
 
